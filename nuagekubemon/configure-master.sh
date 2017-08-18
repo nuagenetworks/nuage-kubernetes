1483f1d7491bbf99b305934731dfbc6cd275b75b
Rohan Parulekar <2k.rohan@gmail.com>
2017-08-17 21:25:13 -0400

    Ansible changes for DS install of VRS and CNI plugin 0.0 (#55)

--- nuagekubemon/configure-master.sh
+++ nuagekubemon/configure-master.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+NET_CONF=""
+MONITOR_CONF=""
 RUNAS=root
 if [ "$1" = "k8s" ]; then
     BIN=/usr/bin/nuagekubemon
     PIDFILE=/var/run/nuagekubemon.pid
     rm -irf /usr/bin/nuage-openshift-monitor
     MONITOR=nuagekubemon
-    TMP_CONF='/tmp/nuagekubemon.yaml'
     MONITOR_CONF='/usr/share/nuagekubemon/nuagekubemon.yaml'
+    NET_CONF='/usr/share/nuagekubemon/net-config.yaml'
 fi
 
 if [ "$1" = "ose" ]; then
@@ -15,7 +17,6 @@ if [ "$1" = "ose" ]; then
     PIDFILE=/var/run/nuage-openshift-monitor.pid
     rm -irf /usr/bin/nuagekubemon
     MONITOR=nuage-openshift-monitor
-    TMP_CONF='/tmp/nuage-openshift-monitor.yaml'
     MONITOR_CONF='/usr/share/nuage-openshift-monitor/nuage-openshift-monitor.yaml'
 fi
 
@@ -26,17 +27,27 @@ fi
 
 # Configuring Nuage monitor yaml file on master nodes using
 # daemon sets
-cat > $TMP_CONF << EOF
+cat > $MONITOR_CONF << EOF
 EOF
-chmod 777 $TMP_CONF
+chmod 777 $MONITOR_CONF
 
 if [ "${NUAGE_MASTER_VSP_CONFIG:-}" != "" ]; then
-cat >$TMP_CONF <<EOF
+cat >$MONITOR_CONF <<EOF
 ${NUAGE_MASTER_VSP_CONFIG:-}
 EOF
 fi
 
-mv $TMP_CONF $MONITOR_CONF
+# Configuring Nuage master netconfig yaml file on master nodes using
+# daemon sets
+if [ "${NUAGE_MASTER_NETWORK_CONFIG:-}" != "" ]; then
+cat > $NET_CONF << EOF
+EOF
+chmod 777 $NET_CONF
+
+cat >$NET_CONF <<EOF
+${NUAGE_MASTER_NETWORK_CONFIG:-}
+EOF
+fi
 
 # Starting Nuage monitor on master nodes
 CMD="$BIN --config=$MONITOR_CONF"
