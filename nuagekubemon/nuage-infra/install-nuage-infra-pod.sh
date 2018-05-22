1a71c0a610fd9b4a3aad8a9252915953a799b8b1
vareti <vareti@ncsu.edu>
2018-05-22 11:45:36 -0400

    evdf specific changes for kubernetes
      * moved nuage infra to standalone repository

--- nuagekubemon/nuage-infra/install-nuage-infra-pod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This prevents Kubernetes from restarting the Nuage infra 
-# pod repeatedly.
-should_sleep=${SLEEP:-"true"}
-echo "Spawning Nuage Infra Pod.  Sleep=$should_sleep"
-while [ "$should_sleep" == "true"  ]; do
-	sleep 10;
-done
