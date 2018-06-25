db6cc52b5e1423bce5f9f32fd13b332d9ea599cf
sareti <siva_teja.areti@nokia.com>
2018-06-25 14:47:58 -0400

    moved nuage infra to standalone repository

--- nuagekubemon/nuage-infra/vendor/github.com/nuagenetworks/libvrsdk/api/port/doc.go
+++
@@ -1,4 +0,0 @@
-/*
-Package port provides constants and data structures to add, remove and modify ports in a Nuage VRS.
-*/
-package port
--- nuagekubemon/nuage-infra/vendor/github.com/nuagenetworks/libvrsdk/ovsdb/doc.go
+++
@@ -1,4 +0,0 @@
-/*
-Package ovsdb enables programming of the Nuage VRS by directly interacting with Nuage OVSDB tables.
-*/
-package ovsdb
